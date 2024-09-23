aa22865510f0578f58646f4faea09febae8b5e2e
OptimusQuidFacit <chimkaemewiseman@gmail.com>
2024-09-23 15:55:14 +0100

    typescript fix 6

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,13 +3,13 @@ import { ComponentProps } from "react";
 import { useFormStatus } from "react-dom";
 import Loader from "./Loader";
 
-const Button = ({...rest}: ComponentProps<any>) => {
+const Button = () => {
     const { pending } = useFormStatus();
     // useEffect(() => {
     //     console.log(pending)
     //   }, [pending]);
     return (
-        <button {...rest} disabled={pending} className="mx-auto text-center mt-10 cursor-pointer component-home-background hover:border-4 border-secondary text-white py-3 px-5 rounded-xl" type="submit">
+        <button disabled={pending} className="mx-auto text-center mt-10 cursor-pointer component-home-background hover:border-4 border-secondary text-white py-3 px-5 rounded-xl" type="submit">
             {pending?<Loader/>:"Submit"}
         </button>
     );
