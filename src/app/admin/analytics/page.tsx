acc4c532ba0680642243eab7cdca6c93b2718fc5
OptimusQuidFacit <chimkaemewiseman@gmail.com>
2024-10-07 13:36:57 +0100

    change of database

--- src/app/admin/analytics/page.tsx
+++ src/app/admin/analytics/page.tsx
@@ -6,15 +6,9 @@ const page = () => {
     return (
         <div className="flex flex-col gap-3 h-full w-full">
             <div className="flex mt-5 items-center">
-                <div className="flex-1">
-                    <FilterButton className="text-start"/>
-                </div>
                 <h1 className="flex-1 text-center text-xl font-semibold justify-self-center">
                     Data Insights
                 </h1>
-                <div className="flex-1">
-
-                </div>
             </div>
             <div className="flex gap-4 h-4/5">
               <Charts/>
